c0b516a9672a9460a70699eca34c20e6c65d66ab
athenge <athenge@qualys.com>
2024-12-12 16:14:56 +0530

    Delete src/main/java/com.qualys.qint/service/delta directory

--- src/main/java/com.qualys.qint/service/delta/DeltaService.java
+++
@@ -1,9 +0,0 @@
-package com.qualys.qint.service.delta;
-
-import com.qualys.qint.model.DeltaMeta;
-
-public interface DeltaService {
-
-    DeltaMeta fetchDelta(String deltaMsg, String profileUuid);
-
-}
