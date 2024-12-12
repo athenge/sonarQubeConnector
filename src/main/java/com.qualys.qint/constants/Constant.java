ef588ce27c204fd67bdb14f15f8bd3a061f5a556
athenge <athenge@qualys.com>
2024-12-12 14:27:15 +0530

    Create Constant.java

---
+++ src/main/java/com.qualys.qint/constants/Constant.java
@@ -0,0 +1,20 @@
+package com.qualys.qint.constants;
+
+public class Constants {
+
+    public static final String OCI = "OCI";
+    public static final String PRIVATE_KEY = "privateKey";
+    public static final String FINGERPRINT = "fingerprint";
+    public static final String USER_ID = "userId";
+    public static final String TENANCY_ID = "tenancyId";
+    public static final String COMPARTMENT_ID = "compartmentId";
+    public static final String FILE_SUFFIX_PEM = ".pem";
+    public static final String REGION = "region";
+    public static final String INVALID_ENTITY_TYPE_REQUEST = "EntityType for the profile is invalid. Please select correct EntityType";
+    public static final String FAILURE_MESSAGE = "Failed to fetch Data from OCI";
+    public static final String FAILED_TO_CREATE_CLIENT = "Failed to create OCI Client";
+    public static final String ENTITY_TYPE = "EntityType";
+    public static final String ENTITY_TYPE_HOST_AND_VULNERABILITY = "Host and Vulnerability";
+    public static final String INVALID_CONNECTOR_TYPE = "Invalid Connector Type, please correct the Connector Type";
+
+}
